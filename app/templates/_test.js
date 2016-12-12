3a9d80db4c5b92c9c1118d691c408af930ae40bc
lwj <1198275198@qq.com>
2016-12-12 14:29:43 +0800

    fix

--- app/templates/_test.js
+++ app/templates/_test.js
@@ -6,7 +6,7 @@ var [func] = require('./index.js');
 
 var expect = chai.expect;
 
-describe('', function() {
+describe('[Test Description]', function() {
 
     it('should export a function', function() {
         expect([func]).to.be.a('function');
